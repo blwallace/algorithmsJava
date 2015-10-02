83a55214a7d7547e41b81cbcccf4ad89a90b78ea
blwallace <blwallace@uchicago.edu>
2015-10-02 15:14:25 -0500

    fixed formatting

--- src/sort_insertion.java
+++ src/sort_insertion.java
@@ -1,22 +1,21 @@
 /**
  * Created by brianwallace on 10/2/15.
  */
+    // high level pseudocode.
+    // A = unsorted input
+    // n = length of A
+    // for i = 2 to n
+    //    key = A[i]                        // key will look back on sorted list and insert
+    //    j = i - 1                         // j is a tracker.  used to look back on elements in array
+    //    while j > 0 & key > A[j]          // if we haven't reached the end and the key is smaller
+    //       A[j + 1] = key                 // push value to next spot in array
+    //       j--                            // reduce tracker to traverse down array
+    //    A[j+1] = key                      // either we traversed to start of array or key is larger than value
+
 public class sort_insertion {
 
     public static void main(String[] args)
     {
-        // high level pseudocode.
-        // A = unsorted input
-        // n = length of A
-        // for i = 2 to n
-        //    key = A[i]                        // key will look back on sorted list and insert
-        //    j = i - 1                         // j is a tracker.  used to look back on elements in array
-        //    while j > 0 & key > A[j]          // if we haven't reached the end and the key is smaller
-        //       A[j + 1] = key                 // push value to next spot in array
-        //       j--                            // reduce tracker to traverse down array
-        //    A[j+1] = key                      // either we traversed to start of array or key is larger than value
-
-
         //actual code
         Integer[] array = {4,3,5,2,1,6};
 
@@ -34,17 +33,11 @@ public class sort_insertion {
             array[j+1] = key;
         }
 
-
         //to output
         for(int i = 0; i < array.length; i++)
         {
             System.out.println(array[i]);
         }
-
-
-
-
-
     }
 
 }
